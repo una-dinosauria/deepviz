04c59b7b0acc351f598a3d017aa2418297a0d5a9
jltmtz <julm@cs.ubc.ca>
2015-12-14 13:04:51 -0800

    Selection and queries are working

--- js/bundle.js
+++ js/bundle.js
@@ -127,9 +127,9 @@ function gridify_tsne() {
 	grididx = 0;
 	for ( i=0; i<per_row; i++) {
 		for ( j=0; j<nrows; j++) {
-			 grid[ grididx ] = [(i*32)+16, (j*32)+16]
-			taken[ grididx ] = false;
-			 grididx++
+			grid[ grididx ] = [(i*32)+16, (j*32)+16]
+				taken[ grididx ] = false;
+			grididx++
 		}
 	}
 	var gridTree = createKDTree( grid );
@@ -147,57 +147,54 @@ function gridify_tsne() {
 		// Create a kd-tree for fast nn
 		var tree = createKDTree( subgrid )
 
-		// Restructure the embedding so that it is gridified
-		imcounter = 0;
-		k = 10;
+			// Restructure the embedding so that it is gridified
+			imcounter = 0;
+		k = 100;
 
 		outer1:
-		for ( i=0; i<nrows; i++ ) {
-			for ( j=0; j<per_row; j++ ) {
+			for ( i=0; i<nrows; i++ ) {
+				for ( j=0; j<per_row; j++ ) {
 
-				if (imcounter >= total_images) {break outer1;}
-				if (assigned[ imcounter ] ){ imcounter++; continue; }
+					if (imcounter >= total_images) {break outer1;}
+					if (assigned[ imcounter ] ){ imcounter++; continue; }
 
-				// Current embedding
-				x = im_embedding[ im_names[imcounter] ][0];
-				y = im_embedding[ im_names[imcounter] ][1];
+					// Current embedding
+					x = im_embedding[ im_names[imcounter] ][0];
+					y = im_embedding[ im_names[imcounter] ][1];
 
-				// Look for the 10 nearest neghbours in the grid
-				nns = tree.knn( [x,y], k );
-				//console.log( nns )
+					// Look for the 10 nearest neghbours in the grid
+					nns = tree.knn( [x,y], k );
 
-				// Traverse the nns and stick to the first one that is free
-				for ( ii=0; ii<k; ii++ ) {
+					// Traverse the nns and stick to the first one that is free
+					for ( ii=0; ii<k; ii++ ) {
 
-					// If this point has already been taken, move on.
-					if ( subtaken[ nns[ii] ] ) { continue; }
+						// If this point has already been taken, move on.
+						if ( subtaken[ nns[ii] ] ) { continue; }
 
-					// Else we have to tag it in the grid structure. Get the actual coordinates there.
+						// Else we have to tag it in the grid structure. Get the actual coordinates there.
 
-					// Assign the points for the embedding
-					im_embedding[ im_names[imcounter] ] = subgrid[ nns[ii] ];
+						// Assign the points for the embedding
+						im_embedding[ im_names[imcounter] ] = subgrid[ nns[ii] ];
 
-					// Find the index in the original grid
-					idx = gridTree.nn( subgrid[nns[ii]] );
+						// Find the index in the original grid
+						idx = gridTree.nn( subgrid[nns[ii]] );
 
-					taken[ idx ]        = true; // Mark it as taken globally
-					subtaken[ nns[ii] ] = true; // Mark it as taken locally
+						taken[ idx ]        = true; // Mark it as taken globally
+						subtaken[ nns[ii] ] = true; // Mark it as taken locally
 
-					assigned[ imcounter ] = true; // Mark the image as assigned
-					//console.log( 'image ' + imcounter + ' assigned');
-					break;
-				}
+						assigned[ imcounter ] = true; // Mark the image as assigned
+						break;
+					}
 
-				imcounter++;
+					imcounter++;
 
-			} // for j<per_row
-		} // for i<nrows
+				} // for j<per_row
+			} // for i<nrows
 
 		n_unassigned = 0;
 		for (i=0; i<total_images; i++) { if( !assigned[i] ){ n_unassigned++ }}
 
 		all_assigned = n_unassigned == 0;
-		console.log( n_unassigned + ' unassigned pics :(' );
 
 		// Make a subgrid with the points in the grid that have not been assigned
 		subgrid = grid.slice(0);
@@ -227,39 +224,41 @@ function gridify_tsne() {
 	// Make a tree with the assigned points
 	assignedTree = createKDTree( assigned_points );
 
+	context.fillRect(0,0,width,height);
+
 	imcounter = 0;
 	outer2:
-	for ( i=0; i<nrows; i++ ) {
-		for ( j=0; j<per_row; j++ ) {
+		for ( i=0; i<nrows; i++ ) {
+			for ( j=0; j<per_row; j++ ) {
 
-			if (imcounter >= total_images) {break outer2;}
+				if (imcounter >= total_images) {break outer2;}
 
-			x = im_embedding[ im_names[imcounter] ][0] - 16;
-			y = im_embedding[ im_names[imcounter] ][1] - 16;
+				x = im_embedding[ im_names[imcounter] ][0] - 16;
+				y = im_embedding[ im_names[imcounter] ][1] - 16;
 
-			if( assigned[imcounter] ){
-				/*** Draw with multiple resizes ***/
-				// Load into external canvas -- down to 64x64
-				octx.drawImage(image,
-				j*imsize, i*imsize, // Read at this position in the tile
-				imsize, imsize, // Read this much from the tile
-				0,0, 64, 64);
+				if( assigned[imcounter] ){
+					/*** Draw with multiple resizes ***/
+					// Load into external canvas -- down to 64x64
+					octx.drawImage(image,
+							j*imsize, i*imsize, // Read at this position in the tile
+							imsize, imsize, // Read this much from the tile
+							0,0, 64, 64);
 
-				// Draw in the display canvas -- down to 32x32
-				context.drawImage( oc,
-				0, 0, 64, 64,
-				x, y, 32, 32);
+					// Draw in the display canvas -- down to 32x32
+					context.drawImage( oc,
+							0, 0, 64, 64,
+							x, y, 32, 32);
 
-				context.strokeStyle='#000000';
-				context.strokeRect(x, y, 32, 32);
-				context.strokeRect(x, y, 32, 32);
-			} else {
-				context.strokeStyle='#ff0000';
+					context.strokeStyle='#000000';
+					context.strokeRect(x, y, 32, 32);
+					context.strokeRect(x, y, 32, 32);
+				} else {
+					context.strokeStyle='#ff0000';
+				}
+
+				imcounter++;
 			}
-
-			imcounter++;
 		}
-	}
 
 	s1.Stop();
 	console.log('Rendering took ' + s1.ElapsedMilliseconds +  'ms' )
@@ -267,14 +266,166 @@ function gridify_tsne() {
 
 image.src = 'imgs/facespics_128/bigtile.jpg';
 
-// Add a mouse listener to the canvas
+// Function that gets indices that sort an array.
+// Inspired from http://stackoverflow.com/a/3730579/1884420
+function sort_indices( the_array ) {
+	// Create a temporary array with indices
+	temp = [];
+	for (var i in the_array) {
+		temp.push([the_array[i], i]);
+	}
+	// Sor the original array
+	temp.sort(function(left, right) {
+		return left[0] > right[0] ? -1 : 1;
+	});
+	// Recover the indices
+	var indices = [];
+	for (var j in temp) {
+		indices.push( temp[j][1] );
+	}
+	return indices;
+};
+
+// === QUERY FUNCTION. GET DISTANCES. SORT. CREATE PLOT ===
 d3.select("#canvas").on("mousedown", function mouseclick() {
 	var mouse = d3.mouse(this);
+
+	// === COMPUTE THE RANKING ===
 	// What is the image that we are hovering on?
-	nn = assignedTree.nn( mouse );
-	console.log( assigned_points[nn], assigned_names[nn] );
-	//d3.event.stopPropagation(this);
+	nn      = assignedTree.nn( mouse );
+	im_name = assigned_names[ nn ];
+
+	// Get the feature of the image
+	feat =  im_features[im_name];
+
+	// Fill the distances with zeros
+	var distances = Array.apply(null, Array(total_images)).map( Number.prototype.valueOf, 0);
+	var d         = 4096; // Dimensionality of each feature
+
+	// Compute a ranking frmo this feature to all the other images
+	for (i=0; i<total_images; i++) {
+		jth_feat = im_features[ im_names[i] ];
+		for(j=0; j<d; j++) {
+			distances[i] += feat[j] * jth_feat[j];
+		}
+	}
+	// Get the sorting indices
+	distances_srted_idx = sort_indices( distances );
+	distances_srtd      = distances.slice(0);
+	distances_srtd.sort( function(a,b){return b-a});
+	//distances_srtd.shift();
+
+
+	var data = [];
+	for (var i=0; i<distances_srtd.length; i++ ) {
+		data.push( {idx: Number(i), distance: distances_srtd[i]} );
+	}
+
+
+	// === CREATE A NEW DIV AND PUT THE IMAGE THERE
+	var div = d3.select("#queries")
+		.append("div")
+		//.attr('style', 'border: solid');
+
+	// Add the image
+	div.append('img')
+		.attr('src','imgs/facespics_128/' + im_name)
+		.attr('style', 'border:solid;')
+
+	// === NOW ACTUALLY DO THE PLOT
+	var margin = {top: 2, right: 5, bottom: 2, left: 30},
+		p_width = 780 - imsize - margin.left - margin.right,
+		p_height = 64 - margin.top - margin.bottom;
+
+	// Set the ranges
+	var x = d3.scale.linear().range([0, p_width]);
+	var y = d3.scale.linear().range([p_height, 0]);
+
+	// Define the axes
+	var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(100);
+	var yAxis = d3.svg.axis().scale(y).orient("left").tickValues([0.25, 0.5, 1]);
+
+	// Define the line
+	var valueline = d3.svg.line()
+		.x(function(d) { return x(d.idx); })
+		.y(function(d) { return y(d.distance); });
+
+	// Add the svg canvas
+	var svg = div
+		.append("svg")
+		.attr("width", p_width + margin.left + margin.right)
+		.attr("height",p_height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			  "translate(" + margin.left + "," + margin.top + ")");
+
+	// Scale the range of the data
+	x.domain(d3.extent(data, function(d) { return d.idx; }));
+	//y.domain(d3.extent(data, function(d) { return d.distance; }));
+	y.domain( [0,0.5] );
+
+	var local_idx = q_idx; // Remember the index of this query.
+
+	// Add the valueline path.
+	console.log( colorbrewer.Set2[6][local_idx] );
+	svg.append("path")
+		.attr("class", "line")
+		.attr("style", 'stroke: ' + colorbrewer.Set2[8][local_idx])
+		.attr("d", valueline(data));
+
+	// Add the X Axis
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + p_height + ")")
+		.call(xAxis);
+
+	// Add the Y Axis
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis);
+
+	// NOW ADD THE BRUSH
+	var brush = d3.svg.brush()
+		.x( x )
+		.extent( g_extent )
+		.clamp( true )
+		.on( "brush", function(){ brushed(local_idx)} ); // Pass local_idx by closure
+
+	q_idx++;
+
+	g_brushes.push( brush );
+
+	svg.append("g")
+		.attr('id', 'brush' + local_idx)
+		.attr('class', 'brush')
+		.call( brush )
+		.selectAll('rect')
+		.attr('height', p_height);
 });
+
+// Keep a list of all the brushes here
+q_idx = 0;
+g_brushes = [];
+g_extent = [0, 50]
+g_domain = g_extent[1] - g_extent[0];
+
+function brushed( idx ) {
+  // Get the extent of the brush that is being updated.
+  g_extent = g_brushes[ idx ].extent();
+  g_extent = g_extent.map( Math.floor  );
+
+  // Prevent brush resizing -- ugly, still has handlers.
+  g_extent[1] = g_extent[0] + g_domain;
+
+  console.log( g_extent )
+
+  // Update all the rest.
+  for( var i=0; i<g_brushes.length; i++ ) {
+    g_brushes[i].extent( g_extent );
+	// Force them to repaint
+  	d3.select("#brush" + i).call( g_brushes[i] );
+  }
+}
 
 // ===  Stopwatch class (http://stackoverflow.com/a/1210765) === /
 StopWatch = function() {
